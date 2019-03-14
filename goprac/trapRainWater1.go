24b6033bff69f0d01beee8f110a5b1470687ce6d
Avery Wong <avwong13@g.ucla.edu>
2019-03-13 17:56:50 -0700

    Update trapRainWater1.go

--- goprac/trapRainWater1.go
+++ goprac/trapRainWater1.go
@@ -1,45 +1,38 @@
 func trap(height []int) int {
-    //horizontal displacement
-    x := 0
-    //total water accumulated 
-    total := 0
-    //max height
-    maxH := 0
-    //tabs of spaces we filled with blocks or rain
-    tab := 0
-    
-    for i, h := range height {
-        //check if there is a valley for rainwater 
-        if (height[i] > height[i-1] && maxH > h && i > 0) {
-            //calculate raintotal
-            calculateRainTotal
-        }
-        if h >= maxH {
-            x = 0
-            tab = 0
-        }
-        x++
-        
+    //we are going to increment from the left and the right, so intiate two increment variables
+    if len(height) < 3 {
+        return 0
     }
     
+    l := 0
+    r := len(height)-1
+    //we need to keep track of the left and right max, since we need to know know how much rain water can we collect at each point
+    lMax := height[l]
+    rMax := height[r]
+    water := 0
+    for l <= r {
+        //we just need to check which one is taller
+        if height[l] < height[r] {
+            //since the right height, which is  currently set to the max on the right, is taller than the left max
+            //we just need to compare with the left max height to determine how much water is on the current height
+            if height[l] < lMax {
+                water += lMax - height[l]
+            } else {
+                lMax = height[l]
+            }
+            l++
+        } else {
+            //since the left height, which is currently set to the max on the left, is taller than the right max
+            //we just need to compare with the right max height to see how much water is on current height
+            if height[r] < rMax {
+                water += rMax - height[r]
+            } else {
+                rMax = height[r]
+            }
+            r--
+        }
+    }
+    return water
 }
 
-func calculateRainTotal(maxH, h, x, tab int) int {
-    return min(maxh, h) * x - tab
-}
 
-func min(a int, b int) int {
-	c := b
-	if a < b {
-		c = a
-	}
-	return c
-}
-
-func max(a int, b int) int {
-	c := b
-	if a > b {
-		c = a
-	}
-	return c
-}
