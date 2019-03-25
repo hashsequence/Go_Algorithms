b284a9cf9a905ba0733aa22320d3f5d920a83355
Avery Wong <avwong13@g.ucla.edu>
2019-03-25 13:22:41 -0700

    Update minWindowSubstring.go

--- goprac/minWindowSubstring.go
+++ goprac/minWindowSubstring.go
@@ -20,11 +20,15 @@ func minWindow(s string, t string) string {
     
     for ; r < len(s); r++ {
         //fmt.Println(r)
+        //use a map to remember the occurrence of each character
         hash_s[s[r]]++
+        //we increment the count if we can have more of that character in the string according to our pattern t
         if hash_t[s[r]] > 0 && hash_s[s[r]] <= hash_t[s[r]] {
             count++
         }
         
+        //if the count has reach the length of t then that means that we now have a window that satisfies the pattern
+        //but may have extra stuff on the left that are useless or duplicates
         if count == len(t) {
            // fmt.Println("count r is ", r, " and l is ", l)
             //get rid of useless ones and duplicates 
