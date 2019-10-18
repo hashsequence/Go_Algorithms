26674c4a77664706d01fb88b2b2a890f44721a4b
Avery Wong <avwong13@g.ucla.edu>
2019-10-18 10:37:02 -0700

    Update 50myPow.go

--- goprac/50myPow.go
+++ goprac/50myPow.go
@@ -10,6 +10,28 @@ func myPow(x float64, n int) float64 {
     return acc
 }
 
+func myPowV2(base float64, n int) float64 {
+    if n == 0 {
+        return 1
+    }
+    if n == 1 {
+        return base
+    }
+    if n == -1 {
+        return 1/base
+    }
+    return myPowV2(base,func() int {
+        if n % 2 == 0 {
+        return n/2 
+        } else {
+            if n < 0 {
+                 return n/2 - 1   
+            } 
+            return n/2 + 1   
+        }
+    }()) * myPowV2(base,n/2)
+}
+
 func abs(n int) int{
     if n < 0 {
         return n * -1
