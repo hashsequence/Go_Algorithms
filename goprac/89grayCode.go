e6fc57fc3e5e752f1c10e6f1e6cbb122b3a4be0f
Avery Wong <avwong13@g.ucla.edu>
2019-10-18 14:13:05 -0700

    Update 89grayCode.go

--- goprac/89grayCode.go
+++ goprac/89grayCode.go
@@ -7,21 +7,11 @@ And Following is 4-bit sequence (n = 4)
   0000 0001 0011 0010 0110 0111 0101 0100 1100 1101 1111 
   1110 1010 1011 1001 1000
   
-basically the problem is to generate the gray sequence with n-bits
 
-where the next element in the squence is a bit flipped
-
-the algorithm is:
-
-base case [0] if n == 0
-base case [0,1] if n== 1
-n >= 2
-iterate i from 2 to n
-    iterate the elements (e) you already have in your sequence backwards
-        for element e you will make the ith msb a one and add this new element to your sequence
-        eg. if e is 010 then add 1 to the left msb to become 110 
-
-
+the key idea is to find the gray sequence for n take the n-1 gray sequence add 0 the left msb and that gives you
+the first half of the n gray sequence then add 1 to the left msb for the second half but add second half in reverse
+ex graycode(2) =   00 01 11 10
+graycode(3) = first half 000, 001, 011, 010   second half 110 111 101 100
 */
 
 func grayCode(n int) []int {
