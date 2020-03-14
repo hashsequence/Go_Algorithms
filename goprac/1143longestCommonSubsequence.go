4aef486cfb5ad781c6c7062def73e86d843f904a
Avery Wong <avwong13@g.ucla.edu>
2020-03-13 23:11:13 -0700

    Update 1143longestCommonSubsequence.go

--- goprac/1143longestCommonSubsequence.go
+++ goprac/1143longestCommonSubsequence.go
@@ -1,52 +1,66 @@
+/*
+1143. Longest Common Subsequence
+Medium
+
+Given two strings text1 and text2, return the length of their longest common subsequence.
+
+A subsequence of a string is a new string generated from the original string with some characters(can be none) deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde" while "aec" is not). A common subsequence of two strings is a subsequence that is common to both strings.
+
+ 
+
+If there is no common subsequence, return 0.
+
+ 
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+ 
+
+Constraints:
+
+1 <= text1.length <= 1000
+1 <= text2.length <= 1000
+The input strings consist of lowercase English characters only.
+
+solution:
+use dp 
+where dp[i][j] means the longestcommonsubsequene between s1[0:i] and s2[0:j]
+*/
 func longestCommonSubsequence(text1 string, text2 string) int {
-    
-    if len(text1) < len(text2) {
-        str := text1
-        text1 = text2
-        text2 = str
+    l1 := len(text1)
+    l2 := len(text2)
+    dp := make([][]int, l1 + 1)
+    for i := range dp {
+        dp[i] = make([]int, l2 + 1)
     }
-    
-    
-    
-    lp := make([][]int,len(text1))
-    for i,_ := range lp {
-        lp[i] = make([]int,len(text2))
-    }
-    for i,_ := range text1 {
-        for j,_ := range text2 {
-            diagonal, left, up := 0, 0, 0
-            if j-1 >= 0 {
-                left = lp[i][j-1]
+    for i := 1; i <= l1; i++ {
+        for j := 1; j <= l2; j++ {
+            if text1[i - 1] == text2[j - 1] {
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
             }
-            if i-1 >= 0 {
-                up = lp[i-1][j]
-            }
-            if i-1 >= 0 && j-1 >= 0 {
-                diagonal = lp[i-1][j-1]
-            }      
-            lp[i][j] = func() int {
-                if text1[i] == text2[j] {
-                    return diagonal + 1
-                } else {
-                    return max(up,left)
-                }
-            }()
-            
-            
         }
     }
-    //fmt.Println(lp)
-    return lp[len(text1)-1][len(text2)-1]
+    return dp[l1][l2]
 }
 
-
-
-func max(arr ...int) int {
-    m := arr[0]
-    for _,val := range arr {
-        if val > m {
-            m = val
-        }
+func max(a, b int) int {
+    if a > b {
+        return a
     }
-    return m
+    return b
 }
